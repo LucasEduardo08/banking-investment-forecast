27cf4b3c963759cc5695e3d453956d1f31fbbff8
Danilo Bruno <danilobsilv@gmail.com>
2024-04-12 19:00:58 -0400

    initial commit

---
+++ src/main/java/com/banking/bankingInvestment/BankingInvestmentApplication.java
@@ -0,0 +1,13 @@
+package com.banking.bankingInvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingInvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingInvestmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/bankingInvestment/BankingInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.bankingInvestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingInvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
